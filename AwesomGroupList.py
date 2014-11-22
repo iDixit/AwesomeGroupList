c6ced72ed862b8d3dee9c0147aea30687378abdd
Sier Violencia <sierisimo@gmail.com>
2014-11-22 17:35:23 -0600

    Added comment_header for new files - Creado el archivo de pruebas

---
+++ AwesomGroupList.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+# Name: AwesomeGroupList 
+#
+# Version: 0.0.1
+#
+# Author: Sinuhe Jaime Valencia
+#
+# Author_email: sierisimo@gmail.com
+#
+# Description: 
+#     Main file for AwesomeGroupLists
+#
+
+if __name__ == "__main__":
+	while True:
+		data = input("Give a url:")
+
+		if (not "http://" in data) and (not "https://" in data):
+			print("Please give a valid url.",end="\n\n")
+			continue
+		
+		size_split = len(data.split("."))
+		if size_split < 2:
+			print("Really, please, give a valid url.")
+			continue
+
+		last_part = data.split("/")[-1]
+		video_code = last_part.split("/")
+		print(video_code)
